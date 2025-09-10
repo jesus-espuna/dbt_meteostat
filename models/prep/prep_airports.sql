a6dae312da14476b30a5ac1ccbe8c1f47eeab10b
Jesús Espuña <jesus@MacBook-Air.local>
2025-09-10 16:26:31 +0200

    adding prep_airports to models>prep

---
+++ models/prep/prep_airports.sql
@@ -0,0 +1,14 @@
+WITH airports_reorder AS (
+    SELECT country
+    	   ,region
+    	   , faa
+    	   , name 
+    	   , lat 
+    	   , lon 
+    	   , alt 
+    	   , tz 
+    	   , dst
+    	   , city 
+    FROM {{ref('staging_airports')}}
+)
+SELECT * FROM airports_reorder
